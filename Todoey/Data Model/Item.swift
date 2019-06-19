694133249ae3605087e3b4cf083b4aad75814bca
loishsu <lois0301@gmail.com>
2019-06-19 17:54:28 +0800

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by 許馨予 on 2019/6/19.
+//  Copyright © 2019 Lois Hsu. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
+
